830d1952e96aeb67d46d3c6d7edf068eb2fd5d59
Bruce Kroeze <bkroeze@gmail.com>
2021-05-01 17:02:39 -0700

    Added CLI options, and far fancier output

--- index.js
+++ index.js
@@ -1,9 +1,96 @@
-var { AaveService } = require('./src/services/AaveService');
-const Aave = new AaveService(true, true);
+const formatDate = require('date-fns/format');
+const yargs = require('yargs');
+const Table = require('cli-table');
+const { AaveService } = require('./lib/services/AaveService');
 
-Aave.getFormattedBalance('0xcccd8e327bf88bC793337E0270D368d814C3c165')
-    .then((summary) => {
-      console.log('-----------------------------------------');
-      console.log(JSON.stringify(summary, null, 2));
-    });
+const YMDDate = (dt) => formatDate(dt, 'yyyy-MMM-dd HH:MM');
 
+function trimDec(val, decimals) {
+  let [whole, fraction] = val.split('.');
+  if (!fraction) {
+    return val;
+  }
+  if (!whole) {
+    whole = 0;
+  }
+  if (fraction.length > decimals) {
+    fraction = fraction.slice(0, decimals);
+  }
+  return `${whole}.${fraction}`;
+}
+
+async function getBalanceCommand (args) {
+  const {
+    address,
+    decimals,
+    matic,
+    format,
+    verbose,
+    usd,
+    warn
+  } = args;
+
+  if (!address) {
+    throw('Missing Address');
+  }
+  const Aave = new AaveService(matic, verbose);
+  const balance = await Aave.getFormattedBalance(address);
+  const { currentLiquidationThreshold, healthFactor } = balance;
+  const ltv = currentLiquidationThreshold / healthFactor;
+    
+  switch (format) {
+    case 'table': {
+      const table = new Table({
+        head: [YMDDate(Date.now()), "Coin", "Amount"]
+      });
+
+      if (usd) {
+        table.push(["Liquidity", "USD", trimDec(balance.totalLiquidityUSD, decimals)]);
+        table.push(["Collateral", "USD", trimDec(balance.totalCollateralUSD, decimals)]);
+        table.push(["Borrows", "USD", trimDec(balance.totalBorrowsUSD, decimals)]);
+      } else {
+        table.push(["Liquidity", "ETH", trimDec(balance.totalLiquidityETH, decimals)]);
+        table.push(["Collateral", "ETH", trimDec(balance.totalCollateralETH, decimals)]);
+        table.push(["Borrows", "ETH", trimDec(balance.totalBorrowsETH, decimals)]);
+      }
+      table.push(["Loans", "LTV max", trimDec(balance.currentLoanToValue, decimals)]);
+      table.push(["", "LTV", `${trimDec(ltv + "", decimals)}`]);
+      table.push(["", "Health", trimDec(healthFactor, decimals)]);
+      console.log(table.toString());
+      break;
+    }
+    default: {
+      console.log(JSON.stringify(balance, null, 2));
+    }
+  }
+  if (warn && healthFactor < warn) {
+    console.log('WARNING, health below threshold!');
+  }
+}
+
+function balanceOptions (yargs) {
+  const defaultAccount = process.env.AAVE_ACCOUNT || '';
+  return yargs
+    .option('address', {
+      alias: 'a',
+      type: 'string',
+      describe: `Account address (default "${defaultAccount}")`,
+      default: defaultAccount
+    })
+    .option('matic', { alias: 'm', type: 'boolean', default: true })
+    .option('decimals', { type: 'number', desc: 'number of decimals to display for totals', default: 4})
+    .option('format', { type: 'string', desc: 'json,table', default: 'table' })
+    .option('usd', { type: 'boolean', desc: 'show values in USD? (otherwise ETH)', default: true })
+    .option('warn', { type: 'number', desc: 'Warn at Health threshold under (default: off)', default: 0})
+    .option('verbose', { alias: 'v', type: 'boolean', default: false });
+}
+
+yargs
+  .command({
+    command: 'balance',
+    desc: 'Get Aave Balance',
+    builder: balanceOptions,
+    handler: getBalanceCommand
+  })
+  .help()
+  .parse();
---
+++ readme.md
@@ -0,0 +1,20 @@
+matic_portfolio
+===================
+
+Description
+-----------
+Scripts to help manage Aave/Curve investments.
+
+Installation
+------------
+
+- Check out code
+- Do `yarn install`
+- run with `node index.js --help` to see options.
+- export an environment variable "AAVE_ACCOUNT" to avoid having to enter your account all the time.
+
+Examples
+--------
+
+- `node index.js balance --format=json`
+  This will query Aave for your balances and give you them in JSON format.
