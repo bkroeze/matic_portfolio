26966de282a036507c19f6934d4d5193ccc9b0aa
Bruce Kroeze <bkroeze@gmail.com>
2021-05-05 16:55:15 -0700

    rates working

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const IFTTT = require('ifttt-webhooks-channel')
+const { evolve } = require('ramda');
 const formatDate = require('date-fns/format');
 const yargs = require('yargs');
 const Table = require('cli-table');
 const { AaveService } = require('./lib/services/AaveService');
 
-const YMDDate = (dt) => formatDate(dt, 'yyyy-MMM-dd HH:MM');
+const YMDDate = (dt) => {
+  try {
+    return formatDate(dt, 'yyyy-MMM-dd HH:MM');
+  } catch (e) {
+    console.error(e);
+    return dt;
+  }
+}
 
 function trimDec(val, decimals) {
   let [whole, fraction] = val.split('.');
@@ -18,6 +26,21 @@ function trimDec(val, decimals) {
     fraction = fraction.slice(0, decimals);
   }
   return `${whole}.${fraction}`;
+}
+
+
+/**
+ * Builds the base commandline switches every command will want.
+ */
+function baseOptions (yargs) {
+  return yargs
+    .option('matic',
+            { alias: 'm', type: 'boolean', default: true })
+    .option('decimals',
+            { type: 'number', desc: 'number of decimals to display for totals', default: 4})
+    .option('verbose',
+            { alias: 'v', type: 'boolean', default: false });
+  
 }
 
 async function getBalanceCommand (args) {
@@ -41,7 +64,7 @@ async function getBalanceCommand (args) {
   const ltv = currentLiquidationThreshold / healthFactor;
     
   switch (format) {
-    case 'table': {
+    case 'table': {      
       const table = new Table({
         head: [YMDDate(Date.now()), "Coin", "Amount"]
       });
@@ -58,6 +81,7 @@ async function getBalanceCommand (args) {
       table.push(["Loans", "LTV max", trimDec(balance.currentLoanToValue, decimals)]);
       table.push(["", "LTV", `${trimDec(ltv + "", decimals)}`]);
       table.push(["", "Health", trimDec(healthFactor, decimals)]);
+      
       console.log(table.toString());
       break;
     }
@@ -75,17 +99,16 @@ async function getBalanceCommand (args) {
   }
 }
 
+/**
+ * Builds the commandline switches needed for the balance command
+ */
 function balanceOptions (yargs) {
   const defaultAccount = process.env.AAVE_ACCOUNT || '';
   const defaultIFTTT = process.env.AAVE_IFTTT || '';
   
-  return yargs
+  return baseOptions(yargs
     .option('address',
             { alias: 'a', type: 'string', describe: 'Account address', default: defaultAccount })
-    .option('matic',
-            { alias: 'm', type: 'boolean', default: true })
-    .option('decimals',
-            { type: 'number', desc: 'number of decimals to display for totals', default: 4})
     .option('format',
             { type: 'string', desc: 'json,table', default: 'table' })
     .option('usd',
@@ -93,9 +116,62 @@ function balanceOptions (yargs) {
     .option('ifttt',
             { type: 'string', desc: 'IFTTT Key, if a warning should trigger an IFTTT action', default: defaultIFTTT})
     .option('warn',
-            { type: 'number', desc: 'Warn at Health threshold under (default: off)', default: 0})
-    .option('verbose',
-            { alias: 'v', type: 'boolean', default: false });
+            { type: 'number', desc: 'Warn at Health threshold under (default: off)', default: 0}));
+}
+
+async function getRatesCommand (args) {
+    const {
+      // coin,
+      days,
+      decimals,
+      matic,
+      format,
+      verbose
+    } = args;
+
+  const Aave = new AaveService(matic, verbose);
+  const rates = await Aave.getFormattedRates(Date.now(), days);
+
+  switch (format) {
+    case 'table': {
+      const table = new Table({
+        head: ["Date", "Coin", "Deposit rate", "Lend rate"]
+      });
+      
+      Object.keys(rates).forEach((timestamp) => {
+        const dateLabel =  YMDDate(timestamp);
+        rates[timestamp].forEach((coinRate) => {
+          table.push([
+            dateLabel,
+            coinRate.symbol,
+            trimDec(coinRate.liquidityRate, decimals),
+            trimDec(coinRate.variableBorrowRate, decimals)
+          ]);
+        });
+      });
+      
+      console.log(table.toString());
+      break;
+    }
+    case 'csv': {
+      
+      break;
+    }
+    default: {
+      console.log(JSON.stringify(rates, null, 2));
+    }
+  }
+}
+
+function rateOptions (yargs) {
+  return baseOptions(yargs
+    .option('days',
+            { type: 'number', describe: 'How many days of data to retrieve', default: 30 })
+        .option('format',
+                { type: 'string', desc: 'json,table,csv', default: 'table' }));
+
+  /* .option('coin',
+   *         { type: 'string', describe: 'Asset name in Aave', default: 'weth' })); */
 }
 
 yargs
@@ -104,6 +180,12 @@ yargs
     desc: 'Get Aave Balance',
     builder: balanceOptions,
     handler: getBalanceCommand
+  })
+  .command({
+    command: 'rates',
+    desc: 'Get Aave historical rates',
+    builder: rateOptions,
+    handler: getRatesCommand
   })
   .help()
   .parse();
