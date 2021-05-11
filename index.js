409ef1a7658adf64448425e9703fe339eba5c44b
Bruce Kroeze <bkroeze@gmail.com>
2021-05-11 11:41:26 -0700

    Added csv output for balance

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const IFTTT = require('ifttt-webhooks-channel')
-const { evolve } = require('ramda');
+const IFTTT = require('ifttt-webhooks-channel');
 const formatDate = require('date-fns/format');
 const yargs = require('yargs');
 const Table = require('cli-table');
@@ -14,9 +13,9 @@ const YMDDate = (dt) => {
     console.error(e);
     return dt;
   }
-}
+};
 
-function trimDec(val, decimals) {
+function trimDec (val, decimals) {
   let [whole, fraction] = val.split('.');
   if (!fraction) {
     return val;
@@ -30,6 +29,21 @@ function trimDec(val, decimals) {
   return `${whole}.${fraction}`;
 }
 
+const getBalanceRecord = (symbol, balance, decimals) => {
+  const { currentLiquidationThreshold, healthFactor } = balance;
+  const ltv = currentLiquidationThreshold / healthFactor;
+
+  return {
+    timestamp: (new Date()).toISOString(),
+    symbol,
+    liquidity: trimDec(balance[`totalLiquidity${symbol}`], decimals),
+    collateral: trimDec(balance[`totalCollateral${symbol}`], decimals),
+    borrows: trimDec(balance[`totalBorrows${symbol}`], decimals),
+    LTVMax: trimDec(balance.currentLoanToValue, decimals),
+    LTV: trimDec(ltv + '', decimals),
+    health: trimDec(healthFactor, decimals)
+  };
+};
 
 /**
  * Builds the base commandline switches every command will want.
@@ -37,12 +51,11 @@ function trimDec(val, decimals) {
 function baseOptions (yargs) {
   return yargs
     .option('matic',
-            { alias: 'm', type: 'boolean', default: true })
+      { alias: 'm', type: 'boolean', default: true })
     .option('decimals',
-            { type: 'number', desc: 'number of decimals to display for totals', default: 4})
+      { type: 'number', desc: 'number of decimals to display for totals', default: 4 })
     .option('verbose',
-            { alias: 'v', type: 'boolean', default: false });
-  
+      { alias: 'v', type: 'boolean', default: false });
 }
 
 async function getBalanceCommand (args) {
@@ -53,38 +66,60 @@ async function getBalanceCommand (args) {
     format,
     verbose,
     usd,
+    eth,
     warn,
+    noheader,
     ifttt
   } = args;
 
   if (!address) {
-    throw('Missing Address');
+    throw new Error('Missing Address');
   }
   const Aave = new AaveService(matic, verbose);
   const balance = await Aave.getFormattedBalance(address);
   const { currentLiquidationThreshold, healthFactor } = balance;
   const ltv = currentLiquidationThreshold / healthFactor;
-    
+
   switch (format) {
-    case 'table': {      
+    case 'table': {
       const table = new Table({
-        head: [YMDDate(Date.now()), "Coin", "Amount"]
+        head: [YMDDate(Date.now()), 'Coin', 'Amount']
       });
 
       if (usd) {
-        table.push(["Liquidity", "USD", trimDec(balance.totalLiquidityUSD, decimals)]);
-        table.push(["Collateral", "USD", trimDec(balance.totalCollateralUSD, decimals)]);
-        table.push(["Borrows", "USD", trimDec(balance.totalBorrowsUSD, decimals)]);
-      } else {
-        table.push(["Liquidity", "ETH", trimDec(balance.totalLiquidityETH, decimals)]);
-        table.push(["Collateral", "ETH", trimDec(balance.totalCollateralETH, decimals)]);
-        table.push(["Borrows", "ETH", trimDec(balance.totalBorrowsETH, decimals)]);
+        table.push(['Liquidity', 'USD', trimDec(balance.totalLiquidityUSD, decimals)]);
+        table.push(['Collateral', 'USD', trimDec(balance.totalCollateralUSD, decimals)]);
+        table.push(['Borrows', 'USD', trimDec(balance.totalBorrowsUSD, decimals)]);
       }
-      table.push(["Loans", "LTV max", trimDec(balance.currentLoanToValue, decimals)]);
-      table.push(["", "LTV", `${trimDec(ltv + "", decimals)}`]);
-      table.push(["", "Health", trimDec(healthFactor, decimals)]);
-      
+
+      if (eth) {
+        table.push(['Liquidity', 'ETH', trimDec(balance.totalLiquidityETH, decimals)]);
+        table.push(['Collateral', 'ETH', trimDec(balance.totalCollateralETH, decimals)]);
+        table.push(['Borrows', 'ETH', trimDec(balance.totalBorrowsETH, decimals)]);
+      }
+      table.push(['Loans', 'LTV max', trimDec(balance.currentLoanToValue, decimals)]);
+      table.push(['', 'LTV', `${trimDec(ltv + '', decimals)}`]);
+      table.push(['', 'Health', trimDec(healthFactor, decimals)]);
+
       console.log(table.toString());
+      break;
+    }
+    case
+    'csv': {
+      const csv = createCsvStringifier({
+        header: ['timestamp', 'symbol', 'liquidity', 'collateral', 'borrows', 'LTVMax', 'LTV', 'health'].map(x => ({ id: x, title: x }))
+      });
+      const records = [];
+      if (usd) {
+        records.push(getBalanceRecord('USD', balance, decimals));
+      }
+      if (eth) {
+        records.push(getBalanceRecord('ETH', balance, decimals));
+      }
+      if (!noheader) {
+        console.log(csv.getHeaderString().slice(0, -1));
+      }
+      console.log(csv.stringifyRecords(records).slice(0, -1));
       break;
     }
     default: {
@@ -96,7 +131,7 @@ async function getBalanceCommand (args) {
     if (ifttt) {
       console.log('Sending to IFTTT');
       const channel = new IFTTT(ifttt);
-      channel.post('aave_health_alert', [trimDec(healthFactor), warn] );
+      channel.post('aave_health_alert', [trimDec(healthFactor), warn]);
     }
   }
 }
@@ -107,30 +142,34 @@ async function getBalanceCommand (args) {
 function balanceOptions (yargs) {
   const defaultAccount = process.env.AAVE_ACCOUNT || '';
   const defaultIFTTT = process.env.AAVE_IFTTT || '';
-  
+
   return baseOptions(yargs
     .option('address',
-            { alias: 'a', type: 'string', describe: 'Account address', default: defaultAccount })
+      { alias: 'a', type: 'string', describe: 'Account address', default: defaultAccount })
     .option('format',
-            { type: 'string', desc: 'json,table', default: 'table' })
+      { type: 'string', desc: 'json,csv,table', default: 'table' })
+    .option('noheader',
+      { type: 'boolean', describe: 'do not output header', default: false })
     .option('usd',
-            { type: 'boolean', desc: 'show values in USD? (otherwise ETH)', default: true })
+      { type: 'boolean', desc: 'show USD values?', default: true })
+    .option('eth',
+      { type: 'boolean', desc: 'show ETH values?', default: false })
     .option('ifttt',
-            { type: 'string', desc: 'IFTTT Key, if a warning should trigger an IFTTT action', default: defaultIFTTT})
+      { type: 'string', desc: 'IFTTT Key, if a warning should trigger an IFTTT action', default: defaultIFTTT })
     .option('warn',
-            { type: 'number', desc: 'Warn at Health threshold under (default: off)', default: 0}));
+      { type: 'number', desc: 'Warn at Health threshold under (default: off)', default: 0 }));
 }
 
 async function getRatesCommand (args) {
-    const {
-      // coin,
-      // days,
-      // hours,
-      decimals,
-      matic,
-      format,
-      verbose,
-    } = args;
+  const {
+    // coin,
+    // days,
+    // hours,
+    decimals,
+    matic,
+    format,
+    verbose
+  } = args;
 
   // until I fix the history query
   const days = 1;
@@ -141,9 +180,9 @@ async function getRatesCommand (args) {
   switch (format) {
     case 'table': {
       const table = new Table({
-        head: ["Date", "Coin", "Deposit rate", "Lend rate"]
+        head: ['Date', 'Coin', 'Deposit rate', 'Lend rate']
       });
-      
+
       Object.keys(rates).forEach((timestamp) => {
         rates[timestamp].forEach((coinRate) => {
           table.push([
@@ -154,32 +193,32 @@ async function getRatesCommand (args) {
           ]);
         });
       });
-      
+
       console.log(table.toString());
       break;
     }
     case 'csv': {
       const csv = createCsvStringifier({
         header: ['timestamp', 'symbol', 'liquidityRate', 'variableBorrowRate'].map(x => ({ id: x, title: x }))
-      })
-      
+      });
+
       const toCSVRow = (timestamp, row) => ({
         timestamp,
         symbol: row.symbol,
         liquidityRate: trimDec(row.liquidityRate, decimals),
         variableBorrowRate: trimDec(row.variableBorrowRate, decimals)
       });
-      
+
       const records = [];
       Object.keys(rates).forEach((timestamp) => {
-        rates[timestamp].forEach((coinRate) => {          
+        rates[timestamp].forEach((coinRate) => {
           records.push(toCSVRow(timestamp, coinRate));
         });
       });
       if (!args.noheader) {
-        console.log(csv.getHeaderString().slice(0,-1));
+        console.log(csv.getHeaderString().slice(0, -1));
       }
-      console.log(csv.stringifyRecords(records).slice(0,-1));
+      console.log(csv.stringifyRecords(records).slice(0, -1));
       break;
     }
     default: {
@@ -195,9 +234,9 @@ function rateOptions (yargs) {
      * .option('hours',
      *         { type: 'boolean', describe: 'Get hours instead of days', default: false }) */
     .option('noheader',
-            { type: 'boolean', describe: 'do not output header', default: false })
+      { type: 'boolean', describe: 'do not output header', default: false })
     .option('format',
-            { type: 'string', desc: 'json,table,csv', default: 'table' }));
+      { type: 'string', desc: 'json,table,csv', default: 'table' }));
 
   /* .option('coin',
    *         { type: 'string', describe: 'Asset name in Aave', default: 'weth' })); */
